837ff4d9458075573d3e7e5f9c3fd307135687be
chandra <javaonlinetraining@gmail.com>
2017-05-09 14:48:57 +0530

    first time modified

---
+++ SalesTool/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	SalesData data = new SalesData();
+	displayGreetings();
+	data.display();
+
+	}
+	
+	public static void displayGreetings()
+	{
+		System.out.println("Welcome to GIT Version controller");
+	}
+
+}
---
+++ SalesTool/src/SalesData.java
@@ -0,0 +1,12 @@
+
+public class SalesData {
+	
+	int[] data = {0,4,12,2};
+	
+	public void display()
+	{
+		for(int i:data)
+		System.out.println("Todo :"+data[i]);
+	}
+
+}
